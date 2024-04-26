fbba74e6029bc4295ee8053702939660bb71ecbd
unknown <reynard.satria@gmail.com>
2024-04-26 11:11:32 +0700

    autoscrol

--- script.js
+++ script.js
@@ -22,3 +22,34 @@ document.getElementById('popup').addEventListener('click', function(event) {
     closePopup();
   }
 });
+
+
+const projectsContainer = document.getElementById('projects');
+
+
+let scrollSpeed = 18;
+let scrollDirection = 1;
+const scrollInterval = 100;
+
+
+function autoScrollProjects() {
+  if (projectsContainer.scrollLeft >= projectsContainer.scrollWidth - projectsContainer.clientWidth) {
+    scrollDirection = -1;
+  } else if (projectsContainer.scrollLeft <= 0) {
+    scrollDirection = 1;
+  }
+
+  projectsContainer.scrollLeft += scrollSpeed * scrollDirection;
+}
+
+const scrollIntervalId = setInterval(autoScrollProjects, scrollInterval);
+
+
+projectsContainer.addEventListener('mouseenter', () => {
+  clearInterval(scrollIntervalId);
+});
+
+
+projectsContainer.addEventListener('mouseleave', () => {
+  scrollIntervalId = setInterval(autoScrollProjects, scrollInterval);
+});
